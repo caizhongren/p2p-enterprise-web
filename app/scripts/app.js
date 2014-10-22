99788449193d7b2f1974619a2fb6bb74ad5d67df
dongyao <dongyao@hoolai.com>
2014-10-22 13:58:03 +0800

    复用js路由设置

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -32,22 +32,6 @@
  			}
  		}
  	})
- 	.state('root.main', {
- 		url: '/',
- 		views: {
- 			'': {
- 				templateUrl: 'views/main.html',
- 				controller: 'MainCtrl',
- 				controllerUrl: 'scripts/controller/main/main-ctrl'
- 			},
- 			'slider': {
- 				templateUrl: 'views/slider.html'
- 			},
- 			'sponsor': {
- 				templateUrl: 'views/project/project-sponsor-list.html'
- 			}
- 		}
- 	})
  	.state('root.login', {
  		url: '/login',
  		views: {
@@ -58,23 +42,234 @@
  			}
  		}
  	})
- 	.state('root.registerMobile', {
- 		url: '/register-mobile',
+ 	/*------------------------------------------  user-center  -----------------------------------------------*/
+ 	.state('root.userCenter', {
  		views: {
- 			'': {
- 				templateUrl: 'views/register/register-mobile.html', 
- 				controller: 'RegisterMobileCtrl', 
- 				controllerUrl: 'scripts/controller/register/register-mobile-ctrl'
+ 			'user-center': {
+ 				templateUrl: 'views/user-center/user-center.html',
+ 				controller: 'UserCenterCtrl',
+ 				controllerUrl: 'scripts/controller/user-center-ctrl'
+ 			},
+ 			'sidebar': {
+ 				templateUrl: 'views/user-center/sidebar.html',
+ 				controller: 'UserCenterCtrl',
+ 				controllerUrl: 'scripts/controller/user-center-ctrl'
  			}
  		}
  	})
- 	.state('root.registerMail', {
- 		url: '/register-mail',
+ 	.state('root.userCenter.account-overview', {
+ 		url: '/account-overview',
+ 		views: {
+ 			'user-center': {
+ 				templateUrl: 'views/user-center/account-overview.html',
+ 				controller: 'AccountOverviewCtrl',
+ 				controllerUrl: 'scripts/controller/user-center/account-overview-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.userCenter.assets-overview', {
+ 		url: '/assets-overview',
+ 		views: {
+ 			'user-center': {
+ 				templateUrl: 'views/user-center/assets-overview.html',
+ 				controller: 'AssetsOverviewCtrl',
+ 				controllerUrl: 'scripts/controller/user-center/assets-overview-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.userCenter.bankcard-management', {
+ 		url: '/bankcard-management',
+ 		views: {
+ 			'user-center': {
+ 				templateUrl: 'views/user-center/bankcard-management.html',
+ 				controller: 'BankCardManagementCtrl',
+ 				controllerUrl: 'scripts/controller/user-center/bankcard-management-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.userCenter.security-settings', {
+ 		url: '/security-settings',
+ 		views: {
+ 			'user-center': {
+ 				templateUrl: 'views/user-center/security-settings.html',
+ 				controller: 'SecuritySettingsCtrl',
+ 				controllerUrl: 'scripts/controller/user-center/security-settings-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.userCenter.recharge', {
+ 		url: '/recharge',
+ 		views: {
+ 			'user-center': {
+ 				templateUrl: 'views/user-center/recharge.html',
+ 				controller: 'RechargeCtrl',
+ 				controllerUrl: 'scripts/controller/user-center/recharge-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.userCenter.withdraw', {
+ 		url: '/withdraw',
+ 		views: {
+ 			'user-center': {
+ 				templateUrl: 'views/user-center/withdraw.html',
+ 				controller: 'WithdrawCtrl',
+ 				controllerUrl: 'scripts/controller/user-center/withdraw-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.userCenter.record', {
+ 		url: '/record',
+ 		views: {
+ 			'user-center': {
+ 				templateUrl: 'views/user-center/record.html',
+ 				controller: 'UserCenterCtrl',
+ 				controllerUrl: 'scripts/controller/user-center/user-center-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.userCenter.investment', {
+ 		url: '/userCenter-investment',
+ 		views: {
+ 			'user-center': {
+ 				templateUrl: 'views/user-center/investment.html',
+ 				controller: 'UserOrderCtrl',
+ 				controllerUrl: 'scripts/controller/user-center/user-order-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.userCenter.investment-query', {
+ 		url: '/userCenter-investment/:dateInterval',
+ 		views: {
+ 			'user-center': {
+ 				templateUrl: 'views/user-center/investment.html',
+ 				controller: 'UserOrderCtrl',
+ 				controllerUrl: 'scripts/controller/user-center/user-order-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.userCenter.news', {
+ 		url: '/news',
+ 		views: {
+ 			'user-center': {
+ 				templateUrl: 'views/user-center/news.html',
+ 				controller: 'UserCenterCtrl',
+ 				controllerUrl: 'scripts/controller/user-center/user-center-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.userCenter.realname-authentication', {
+ 		url: '/realname-authentication',
+ 		views: {
+ 			'user-center': {
+ 				templateUrl: 'views/user-center/realname-authentication.html',
+ 				controller: 'UserCenterCtrl',
+ 				controllerUrl: 'scripts/controller/user-center/user-center-ctrl'
+ 			}
+ 		}
+ 	})
+ 	/*---------------------------------------------  yeepay  ---------------------------------------------*/
+ 	.state('root.userCenter.yeepay-callback', {
+ 		url: '/yeepay-callback/:yeepayService/:yeepayStatus',
  		views: {
  			'': {
- 				templateUrl: 'views/register/register-mail.html', 
- 				controller: 'RegisterMailCtrl', 
- 				controllerUrl: 'scripts/controller/register/register-mail-ctrl'
+ 				templateUrl: 'views/user-center/yeepay-callback.html',
+ 				controller: 'YeepayCtrl',
+ 				controllerUrl: 'scripts/controller/yeepay/yeepay-ctrl'
+ 			}
+ 		}
+ 	})
+ 	/*------------------------------------------  about-us  -----------------------------------------------*/
+ 	.state('root.about-us', {
+ 		views: {
+ 			'about-us': {
+ 				templateUrl: 'views/about-us/about-us.html',
+ 				controller: 'AboutUsCtrl',
+ 				controllerUrl: 'scripts/controller/about-us/about-us-ctrl'
+ 			},
+ 			'about-sidebar': {
+ 				templateUrl: 'views/about-us/about-sidebar.html',
+ 				controller: 'AboutUsCtrl',
+ 				controllerUrl: 'scripts/controller/about-us/about-us-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.about-us.introduction-of-platform', {
+ 		url: '/introduction-of-platform',
+ 		views: {
+ 			'about-us-right': {
+ 				templateUrl: 'views/about-us/introduction-of-platform.html',
+ 				controller: 'IntroductionCtrl',
+ 				controllerUrl: 'scripts/controller/about-us/about-us-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.about-us.business-model', {
+ 		url: '/business-model',
+ 		views: {
+ 			'about-us-right': {
+ 				templateUrl: 'views/about-us/business-model.html',
+ 				controller: 'BusinessModelCtrl',
+ 				controllerUrl: 'scripts/controller/about-us/about-us-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.about-us.company-profile', {
+ 		url: '/company-profile',
+ 		views: {
+ 			'about-us-right': {
+ 				templateUrl: 'views/about-us/company-profile.html',
+ 				controller: 'CompanyProfileCtrl',
+ 				controllerUrl: 'scripts/controller/about-us/about-us-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.about-us.web-site-announcement', {
+ 		url: '/web-site-announcement',
+ 		views: {
+ 			'about-us-right': {
+ 				templateUrl: 'views/about-us/web-site-announcement.html',
+ 				controller: 'WebSiteCtrl',
+ 				controllerUrl: 'scripts/controller/about-us/about-us-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.about-us.hongcaidynamic', {
+ 		url: '/hongcaidynamic',
+ 		views: {
+ 			'about-us-right': {
+ 				templateUrl: 'views/about-us/hongcaidynamic.html',
+ 				controller: 'HongcaiDynamicCtrl',
+ 				controllerUrl: 'scripts/controller/about-us/about-us-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.about-us.media-reports', {
+ 		url: '/media-reports',
+ 		views: {
+ 			'about-us-right': {
+ 				templateUrl: 'views/about-us/media-reports.html',
+ 				controller: 'MediaReportsCtrl',
+ 				controllerUrl: 'scripts/controller/about-us/about-us-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.about-us.link-us', {
+ 		url: '/link-us',
+ 		views: {
+ 			'about-us-right': {
+ 				templateUrl: 'views/about-us/link-us.html',
+ 				controller: 'LinkUsCtrl',
+ 				controllerUrl: 'scripts/controller/about-us/about-us-ctrl'
+ 			}
+ 		}
+ 	})
+ 	.state('root.about-us.news-details', {
+ 		url: '/news-details',
+ 		views: {
+ 			'about-us-right': {
+ 				templateUrl: 'views/about-us/news-details.html',
+ 				controller: 'NewsCtrl',
+ 				controllerUrl: 'scripts/controller/about-us/about-us-ctrl'
  			}
  		}
  	})
