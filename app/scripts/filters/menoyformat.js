265651d45fe97ca483689d6e4054921ad11e3c23
wangkaijie <wangkaijie@hoolai.com>
2014-11-20 13:39:22 +0800

    filters 定义超过多少钱后显示万元。

---
+++ app/scripts/filters/menoyformat.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name p2pEnterpriseWebApp.filter:menoyFormat
+ * @function
+ * @description
+ * # menoyFormat
+ * Filter in the p2pEnterpriseWebApp.
+ */
+angular.module('hongcaiApp')
+  .filter('menoyFormat', function () {
+    return function (input, formatNo) {
+      input = input || '';
+      if(out.length > formatNo ){
+        return (input/10000).toString() + "万";
+      } else {
+        return;
+      }
+    };
+  });
