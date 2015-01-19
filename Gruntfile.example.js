6e33b371d10427bd575aa0104e2391768aec5305
wangkaijie <wangkaijie@hoolai.com>
2015-01-19 15:57:26 +0800

    修改默认的Gruntfile.example.js 配置文件

--- Gruntfile.example.js
+++ Gruntfile.example.js
@@ -112,7 +112,7 @@ module.exports = function(grunt) {
       proxies: [{
         context: '/enterprise/api/v1',
         host: '192.168.1.43',
-        port: 8000
+        port: 8080
       }],
       livereload: {
         options: {
