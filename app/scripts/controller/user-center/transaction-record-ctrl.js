24ddcb04edae1625018a9d5966a6e81314ba2586
wangkaijie <wangkaijie@hoolai.com>
2015-01-20 11:21:34 +0800

    JSHINT。

--- app/scripts/controller/user-center/transaction-record-ctrl.js
+++ app/scripts/controller/user-center/transaction-record-ctrl.js
@@ -1,30 +1,40 @@
-hongcaiApp.controller('TransactionRecordCtrl', ['$scope', '$state', '$rootScope', '$stateParams', 'UserCenterService', '$location',  function ($scope, $state, $rootScope,  $stateParams,  UserCenterService, $location) {
-  $rootScope.selectSide = 'transaction-record';
-  $scope.type = $stateParams.type || 0;
-  $scope.dateInterval = $stateParams.dateInterval || 0;
-  $scope.typeValue = { '1': '充值', '2': '提现', '3': '投资', '4': '收益', '5': '回收本金', '6': '放款','7': '还款','8': '手续费' };
-  var getDealByUser = UserCenterService.getDealListByUser.get({ dateInterval: $stateParams.dateInterval,type: $stateParams.type},function() {
-    if (getDealByUser.ret === 1) {
-      $scope.dealList = getDealByUser.data.dealList;
-      $scope.type = getDealByUser.data.type;
-      $scope.dateInterval = getDealByUser.data.dateInterval;
-      $scope.userId = getDealByUser.data.userId;
-      $scope.capital = getDealByUser.data.capital;
-      $scope.currentPage = 0;
-      $scope.pageSize = 10;
-      $scope.data = [];
-      $scope.numberOfPages = function() {
-        return Math.ceil($scope.data.length / $scope.pageSize);
-      };
-      for (var i = 0; i < $scope.dealList.length; i++) {
-        $scope.data.push($scope.dealList[i]);
+'use strict';
+angular.module('hongcaiApp')
+  .controller('TransactionRecordCtrl', ['$scope', '$state', '$rootScope', '$stateParams', 'UserCenterService', function($scope, $state, $rootScope, $stateParams, UserCenterService) {
+    $rootScope.selectSide = 'transaction-record';
+    $scope.type = $stateParams.type || 0;
+    $scope.dateInterval = $stateParams.dateInterval || 0;
+    $scope.typeValue = {
+      '1': '充值',
+      '2': '提现',
+      '3': '投资',
+      '4': '收益',
+      '5': '回收本金',
+      '6': '放款',
+      '7': '还款',
+      '8': '手续费'
+    };
+    var getDealByUser = UserCenterService.getDealListByUser.get({
+      dateInterval: $stateParams.dateInterval,
+      type: $stateParams.type
+    }, function() {
+      if (getDealByUser.ret === 1) {
+        $scope.dealList = getDealByUser.data.dealList;
+        $scope.type = getDealByUser.data.type;
+        $scope.dateInterval = getDealByUser.data.dateInterval;
+        $scope.userId = getDealByUser.data.userId;
+        $scope.capital = getDealByUser.data.capital;
+        $scope.currentPage = 0;
+        $scope.pageSize = 10;
+        $scope.data = [];
+        $scope.numberOfPages = function() {
+          return Math.ceil($scope.data.length / $scope.pageSize);
+        };
+        for (var i = 0; i < $scope.dealList.length; i++) {
+          $scope.data.push($scope.dealList[i]);
+        }
+      } else {
+        console.log('ask record, why getDealByUser did not load data...');
       }
-    } else {
-      console.log('ask record, why getDealByUser did not load data...');
-    }
-  });
-}]);
-
-
-
-
+    });
+  }]);
