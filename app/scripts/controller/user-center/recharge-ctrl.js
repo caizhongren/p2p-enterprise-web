ad2fc598dee533876b1d6dd0e7049e5864490b84
yelun <yelun1@hoolai.com>
2017-06-23 15:27:40 +0800

    fix(支付方式) 修改充值快捷支付方式易宝支付改为先锋支付

--- app/scripts/controller/user-center/recharge-ctrl.js
+++ app/scripts/controller/user-center/recharge-ctrl.js
@@ -48,7 +48,7 @@ angular.module('hongcaiApp')
       $scope.payment = payment;
       if(payment ===1){
         $scope.rechargeWay = 'SWIFT';
-        $scope.expectPayCompany = 'YEEPAY';
+        $scope.expectPayCompany = 'UCFPAY';
       }else {
         $scope.rechargeWay = 'WEB';
         $scope.expectPayCompany = 'YEEPAY';
